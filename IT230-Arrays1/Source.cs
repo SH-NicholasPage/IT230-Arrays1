1f3edb3f99805f7fa5a35d7479b01c0e1e952239
Nicholas Page <npage@southhills.edu>
2023-08-16 23:48:06 -0400

    Initial commit

---
+++ IT230-Arrays1/Source.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+//DO NOT USE LINQ!
+
+namespace IT230_Arrays1
+{
+    public class Source
+    {
+        public static int[] players; //Do not initialize the array here
+        
+        public static void InsertIntoArray(int number)
+        {
+            //TODO: Append the number passed in to the array
+        }
+        
+        public static bool SearchInArray(int number)
+        {
+            //TODO: Return true if the number passed in exists in the array, otherwise return false
+            return false;
+        }
+        
+        public static void DeleteFromArray(int number)
+        {
+            //TODO: Delete the number passed in from the array by replacing it with 0. If number does not exist in the array, do nothing.
+            //Remember, array elements must be contiguous. Do not allow 0s between non-0 elements.
+            //0s mean no player exists at that position in the array.
+            //Do not shrink the array.
+        }
+    }
+}
